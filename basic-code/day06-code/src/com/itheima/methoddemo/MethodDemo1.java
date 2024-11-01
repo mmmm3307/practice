46e0cee4f8a23972343da77f5ac82a451c8bb9a2
DELL <3217177847@qq.com>
2024-11-01 17:52:13 +0800

    add

---
+++ basic-code/day06-code/src/com/itheima/methoddemo/MethodDemo1.java
@@ -0,0 +1,14 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo1 {
+    public static void main(String[] args) {
+//        方法定义与调用
+        playGame();
+    }
+
+    public static void playGame() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
---
+++ basic-code/day06-code/src/com/itheima/methoddemo/MethodDemo2.java
@@ -0,0 +1,14 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+//        目标：利用方法打印
+        printInfo();
+    }
+
+    public static void printInfo() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
