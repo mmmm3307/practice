9cbe326cfa3eb2f9a1f0859124eb1bfd14079307
DELL <3217177847@qq.com>
2024-12-04 18:25:28 +0800

    add

--- basic-code/oop-test/src/com/itheima/test3/Test.java
+++ basic-code/oop-test/src/com/itheima/test3/Test.java
@@ -2,6 +2,15 @@ package com.itheima.test3;
 
 public class Test {
     public static void main(String[] args) {
+/*      键盘录入
+        第一套体系：
+        nextInt();接收整数
+        nextDouble();接收小数
+        next();接收字符串
 
+        第二套体系：
+        nextLine();接受字符串
+        可以接收空格，制表符，遇到回车才停止接收数据
+        */
     }
 }
